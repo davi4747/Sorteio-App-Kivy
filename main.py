ef101878868a560e2297f2dcee516cae0d9f0a58
Davi Sousa <desenvolvedordavi47@gmail.com>
2018-09-18 11:51:03 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.label import Label
 from kivy.properties import ListProperty
+from kivy.metrics import dp
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from kivy.core.window import Window
@@ -19,11 +20,22 @@ class Gerenciador(ScreenManager):
 
 class Main_menu(Screen):
 
+    def on_pre_enter(self, *args):
+        Window.bind(on_keyboard=self.voltar)
+
+    def voltar(self, window, key, *args):
+        if key == 27:
+            self.confirmacao()
+            return True
+
+    def on_pre_leave(self, *args):
+        Window.unbind(on_keyboard=self.voltar)
+
     def confirmacao(self):
-        box = BoxLayout(orientation='vertical', spacing=30)
+        box = BoxLayout(orientation='vertical', spacing=dp(3))
         atencao = Image(source='att.png')
-        botoes = BoxLayout(padding=10, spacing=30)
-        pop = Popup(title='Você quer mesmo sair?', content=box, size_hint=(None, None), size=(800, 500))
+        botoes = BoxLayout(padding=dp(4), spacing=dp(3))
+        pop = Popup(title='Você quer mesmo sair?', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
         sim = Botaor(text='SIM', on_release=App.get_running_app().stop)
         nao = Botaor(text='NÃO', on_release=pop.dismiss)
         botoes.add_widget(sim)
@@ -98,10 +110,10 @@ class Nomes(Screen):
         arquivo = open('lists.txt', 'r')
         arquivo2 = arquivo.read()
         if arquivo2 == '' or arquivo2 == '{}':
-            box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-            pop = Popup(title='Listas Salvas', content=box, size_hint=(None, None), size=(800, 500))
+            box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+            pop = Popup(title='Listas Salvas', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
             atencao = Image(source='att.png')
-            box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+            box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
             aviso = Label(text='Não há lista salva!')
             box2.add_widget(atencao)
             box2.add_widget(aviso)
@@ -129,6 +141,9 @@ class Nomes(Screen):
             self.ids.box_participantes.add_widget(Participante(c))
         self.lista_titulos = []
         App.get_running_app().root.get_screen('load').ids.box_titulos.clear_widgets()
+        q = len(self.lista_nomes_temp)
+        aviso = str(q) + ' PARTICIPANTES:'
+        App.get_running_app().root.get_screen('menu_nomes').ids.participantes.text = aviso
         App.get_running_app().root.current = 'menu_nomes'
 
     def removert(self, titulo):
@@ -153,10 +168,10 @@ class Nomes(Screen):
 
     def verificar(self):
         if len(self.lista_nomes_temp) <= 1:
-            box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-            pop = Popup(title='Salvar Lista', content=box, size_hint=(None, None), size=(800, 500))
+            box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+            pop = Popup(title='Salvar Lista', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
             atencao = Image(source='att.png')
-            box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+            box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
             aviso = Label(text='Adicione ao menos 2 participantes!')
             box2.add_widget(atencao)
             box2.add_widget(aviso)
@@ -245,10 +260,10 @@ class Resultado(Screen):
 
     def preparar(self):
         if len(App.get_running_app().root.get_screen('menu_nomes').lista_nomes_temp) <= 1:
-            box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-            pop = Popup(title='CONFIRMAR SORTEIO', content=box, size_hint=(None, None), size=(800, 500))
+            box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+            pop = Popup(title='CONFIRMAR SORTEIO', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
             atencao = Image(source='att.png')
-            box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+            box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
             aviso = Label(text='Adicione ao menos 2 participantes!')
             box2.add_widget(atencao)
             box2.add_widget(aviso)
@@ -265,10 +280,10 @@ class Resultado(Screen):
 
     def grupos(self):
         if len(App.get_running_app().root.get_screen('menu_nomes').lista_nomes_temp) <= 2:
-            box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-            pop = Popup(title='CONFIRMAR SORTEIO', content=box, size_hint=(None, None), size=(800, 500))
+            box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+            pop = Popup(title='CONFIRMAR SORTEIO', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
             atencao = Image(source='att.png')
-            box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+            box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
             aviso = Label(text='Adicione ao menos 3 participantes!')
             box2.add_widget(atencao)
             box2.add_widget(aviso)
@@ -353,7 +368,7 @@ class Sorteadog(BoxLayout):
         super(Sorteadog, self).__init__()
         self.ids.ordem.text = ordem
         for c in escolhidos:
-            self.ids.escolhidos.add_widget(Label(text=c, font_size=47, size_hint_y=None, height=55))
+            self.ids.escolhidos.add_widget(Label(text=c, font_size=dp(18), size_hint_y=None, height=dp(28)))
 
 class Participante(BoxLayout):
     def __init__(self, texto, **kwargs):
@@ -373,11 +388,19 @@ class Numeros(Screen):
     def on_pre_leave(self, *args):
         Window.unbind(on_keyboard=self.voltar)
 
+    def ok1(self):
+        self.ids.number1.text = self.ids.text1.text.strip()
+        self.ids.text1.text = ''
+
+    def ok2(self):
+        self.ids.number2.text = self.ids.text2.text.strip()
+        self.ids.text2.text = ''
+
     def avisar(self):
-        box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-        pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(800, 500))
+        box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+        pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
         atencao = Image(source='att.png')
-        box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+        box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
         aviso = Label(text='Digite apenas números!')
         box2.add_widget(atencao)
         box2.add_widget(aviso)
@@ -386,43 +409,15 @@ class Numeros(Screen):
         box.add_widget(box2)
         pop.open()
 
-    def aumentar1(self):
-        try:
-            num = int(self.ids.text1.text.strip()) + 1
-            self.ids.text1.text = str(num)
-        except:
-            self.avisar()
-
-    def aumentar2(self):
-        try:
-            num = int(self.ids.text2.text.strip()) + 1
-            self.ids.text2.text = str(num)
-        except:
-            self.avisar()
-
-    def diminuir1(self):
-        try:
-            num = int(self.ids.text1.text.strip()) - 1
-            self.ids.text1.text = str(num)
-        except:
-            self.avisar()
-
-    def diminuir2(self):
-        try:
-            num = int(self.ids.text2.text.strip()) - 1
-            self.ids.text2.text = str(num)
-        except:
-            self.avisar()
-
     def sortear(self):
         try:
-            numb1 = int(self.ids.text1.text.strip())
-            numb2 = int(self.ids.text2.text.strip())
+            numb1 = int(self.ids.number1.text.strip())
+            numb2 = int(self.ids.number2.text.strip())
             if numb2 <= numb1:
-                box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-                pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(800, 500))
+                box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+                pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
                 atencao = Image(source='att.png')
-                box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+                box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
                 aviso = Label(text='O segundo deve ser maior!')
                 box2.add_widget(atencao)
                 box2.add_widget(aviso)
@@ -478,10 +473,10 @@ class Grupo(Screen):
             for c in App.get_running_app().root.get_screen('resultado').lista_perm:
                 App.get_running_app().root.get_screen('resultado').lista_temp.append(c)
             if num >= len(App.get_running_app().root.get_screen('resultado').lista_temp):
-                box = BoxLayout(orientation='vertical', spacing=15, padding=17)
-                pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(800, 560))
+                box = BoxLayout(orientation='vertical', spacing=dp(4), padding=dp(6))
+                pop = Popup(title='VERIFICAR NÚMEROS', content=box, size_hint=(None, None), size=(dp(250), dp(180)))
                 atencao = Image(source='att.png')
-                box2 = BoxLayout(orientation='vertical', spacing=13, padding=10)
+                box2 = BoxLayout(orientation='vertical', spacing=dp(3), padding=dp(5))
                 aviso = Label(text='Digite um número menor que\na quantidade de participantes!')
                 box2.add_widget(atencao)
                 box2.add_widget(aviso)
