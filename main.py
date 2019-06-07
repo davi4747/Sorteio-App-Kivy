376f3d410f459dd50ca3392ed3f9a31c627a88c1
Davi Sousa <dev.davi47@gmail.com>
2019-06-07 14:41:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Made by Davi Sousa, E-mail: devdavi47@gmail.com, Github Profile: github.com/davig_sousa
+# Made by Davi Sousa, E-mail: devdavi47@gmail.com, Github Profile: github.com/davig-sousa
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
